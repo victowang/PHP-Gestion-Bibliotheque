5e17c4983cbccb45c3249f69bdeba674b08ecd3e
Victor Wang <victorw@hotmail.fr>
2020-01-26 12:57:52 +0100

    Init

---
+++ index.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="utf-8">
+    <title> BE PHP </title>
+</head>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<body>
+    <h1> Bienvenue ! </h1>
+
+    <footer>
+        <div class="alignLeft">Mineure Développement Web : BE PHP</div>
+        <div class="alignRight">Camille BRETTEVILLE, Victor WANG</div>
+    </footer>
+</body>
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# BE PHP
+
+Application de gestion de librairie en ligne avec LAMP  
+
+URL : http://localhost/devweb/  

ed00ae6afbbdfc2e92ab51f3d0baee12f16660c7
Victor Wang <victorw@hotmail.fr>
2020-02-07 15:04:59 +0100

    Destruction automatique d'une commande non validee vide; creation automatique d'une commande vide si aucune n'existe

--- livres.php
+++ livres.php
@@ -27,22 +27,73 @@
         <?php
             include("config.php");
             session_start();
+
+            if (($_SERVER["REQUEST_METHOD"] === "POST") and $_POST['add']==="Ajouter au panier") {
+                $idCommande = $_POST['idcmd'];
+                print_r($idCommande);
+                $idOuvrage = $_POST['idOuvrage'];
+                $quantite = $_POST['qte'];
+                $requete = "SELECT COUNT(*) FROM `lignescmd` WHERE idcmd = '$idCommande' AND idouvrage='$idOuvrage'"; // on verifie s'il y a plus d'une ligne correspondant a cet ouvrage et cette commande
+                $statement = $pdo->query($requete);
+                $count = $statement->fetch()['COUNT(*)'];
+                if ($count === '0') {
+                    $requete = "INSERT INTO `lignescmd` (idcmd, idouvrage, qte) VALUES ('$idCommande', '$idOuvrage', '$quantite');";
+                    $statement = $pdo->query($requete);
+                } 
+                elseif ($count === '1') {
+                    $requete = "UPDATE `lignescmd` SET qte = qte + '$quantite' WHERE idcmd = '$idCommande' AND idouvrage='$idOuvrage'";
+                    $statement = $pdo->query($requete);
+                }
+                else {
+                    $requete = "SELECT SUM(qte) FROM `lignescmd` WHERE idcmd = '$idCommande' AND idouvrage='$idOuvrage'";
+                    $statement = $pdo->query($requete);
+                    $total = $statement->fetch()['SUM(qte)'] + $quantite;
+                    $requete = "DELETE FROM `lignescmd` WHERE idcmd = '$idCommande' AND idouvrage='$idOuvrage';";
+                    print_r($requete);
+                    $statement = $pdo->query($requete);
+                    print_r($statement);
+                    $requete = "INSERT INTO `lignescmd` (idcmd, idouvrage, qte) VALUES ('$idCommande', '$idOuvrage', '$total');";
+                    $statement = $pdo->query($requete);
+                }
+
+            }
+
             $status = "";
             $requete = "SELECT * FROM `ouvrages`";
             $statement = $pdo->query($requete);
             $arrayResultat = $statement->fetchAll();
+            /*
+            if (!isset($Quantities)){
+                $requete = "SELECT SUM(qte) as qte, maCommande.idouvrage as idouvrage FROM (SELECT idouvrage, qte, commandes.idcmd FROM `commandes` JOIN `lignescmd` ON commandes.idcmd=lignescmd.idcmd WHERE idpersonne=".$_SESSION["user_id"]." AND commandes.validee=0) AS maCommande JOIN `ouvrages`ON maCommande.idouvrage=ouvrages.idouvrage GROUP BY titre, auteur, prix, idouvrage, idcmd;";
+                $statement = $pdo->query($requete);
+                $Commande = $statement->fetchAll();
+                print_r($Commande);
+                $Quantities = array();
+            }
+            */
+            $requete = "SELECT (EXISTS (SELECT idcmd FROM `commandes` WHERE idpersonne=".$_SESSION['user_id']." AND validee='0'))";
+            $statement = $pdo->query($requete);
+            $commande = $statement->fetch()[0];
+            if ($commande === '0') {
+                $insertion = "INSERT INTO `commandes` (idpersonne, date, validee) VALUES (".$_SESSION['user_id'].", CURDATE(), '0')";
+                $statement = $pdo->query($insertion);
+            }
+            $requete = "SELECT idcmd FROM `commandes` WHERE idpersonne=".$_SESSION['user_id']." AND validee='0'";
+            $statement = $pdo->query($requete);
+            $idCommande = $statement->fetch()['idcmd'];
+
             foreach ($arrayResultat as $livre){
+                //array_push($Quantities, [$livre['idouvrage'] > 0]);
                 echo '<tr>';
                 echo '<td>'.$livre['titre'].'</td>';
                 echo '<td>'.$livre['auteur'].'</td>';
                 echo '<td>'.$livre['prix'].'€</td>';
                 echo '<td>
-                        <form>
-                            <input type = "submit" value = "-">
-                            Qt
-                            <input type = "submit" value = "+">
-
-                            <input type = "submit" value = "Ajouter">
+                        <form method="POST">
+                            <input type = "hidden" name="idcmd" value="'.$idCommande.'">
+                            <input type = "hidden" name="idOuvrage" value="'.$livre['idouvrage'].'">
+                            <input type="number" step="1" name="qte" min="1" required/>
+                            <input type="submit" name = "add" value = "Ajouter au panier">
                         </form>
                     </td>';
                 echo '</tr>';
--- monPanier.php
+++ monPanier.php
@@ -22,7 +22,6 @@ session_start();
         } 
         else 
         {   
-            //TODO : Faire fonctionner ça
             if (($_SERVER["REQUEST_METHOD"] === "POST") and $_POST['Panier']==="Valider la commande") {
                 $requete = "UPDATE `commandes` SET validee=1 WHERE idpersonne=".$_SESSION['user_id']." AND validee=0";
                 print_r($requete);
@@ -37,7 +36,39 @@ session_start();
         ?>
         <h1>Mon Panier</h1>
         <?php
-            $requete = "SELECT titre, auteur, prix, SUM(qte) as qte, idcmd FROM (SELECT idouvrage, qte, commandes.idcmd FROM `commandes` JOIN `lignescmd` ON commandes.idcmd=lignescmd.idcmd WHERE idpersonne=".$_SESSION["user_id"]." AND commandes.validee=0) AS maCommande JOIN `ouvrages`ON maCommande.idouvrage=ouvrages.idouvrage GROUP BY titre, auteur, prix, idcmd;";
+            if (($_SERVER["REQUEST_METHOD"] === "POST") and $_POST['remove']==="Retirer du panier") {
+                $idCommande = $_POST['idcmd'];
+                $idOuvrage = $_POST['idOuvrage'];
+                $quantite = -$_POST['qte'];
+
+                $requete = "SELECT SUM(qte) FROM `lignescmd` WHERE idcmd = '$idCommande' AND idouvrage='$idOuvrage'";
+                $statement = $pdo->query($requete);
+                $total = $statement->fetch()['SUM(qte)'] + $quantite;
+                if ($total === 0) {
+                    $requete = "DELETE FROM `lignescmd` WHERE idcmd='$idCommande' AND idouvrage='$idOuvrage';";
+                    $statement = $pdo->query($requete);
+                }
+                else {
+                    $requete = "SELECT COUNT(*) FROM `lignescmd` WHERE idcmd = '$idCommande' AND idouvrage='$idOuvrage'"; // on verifie s'il y a plus d'une ligne correspondant a cet ouvrage et cette commande
+                    $statement = $pdo->query($requete);
+                    $count = $statement->fetch()['COUNT(*)'];
+                    if ($count === '1') {
+                        $requete = "UPDATE `lignescmd` SET qte = qte + '$quantite' WHERE idcmd = '$idCommande' AND idouvrage='$idOuvrage'";
+                        $statement = $pdo->query($requete);
+                    }
+                    else {
+
+                        $requete = "DELETE FROM `lignescmd` WHERE idcmd = '$idCommande' AND idouvrage='$idOuvrage';";
+                        print_r($requete);
+                        $statement = $pdo->query($requete);
+                        print_r($statement);
+                        $requete = "INSERT INTO `lignescmd` (idcmd, idouvrage, qte) VALUES ('$idCommande', '$idOuvrage', '$total');";
+                        $statement = $pdo->query($requete);
+                    }
+                }
+                
+            }
+            $requete = "SELECT titre, auteur, prix, SUM(qte) as qte, idcmd, maCommande.idouvrage FROM (SELECT idouvrage, qte, commandes.idcmd FROM `commandes` JOIN `lignescmd` ON commandes.idcmd=lignescmd.idcmd WHERE idpersonne=".$_SESSION["user_id"]." AND commandes.validee=0) AS maCommande JOIN `ouvrages`ON maCommande.idouvrage=ouvrages.idouvrage GROUP BY titre, auteur, prix, idcmd, idouvrage;";
             $statement = $pdo->query($requete);
             $arrayResultat = $statement->fetchAll();
             if (!empty($arrayResultat)) {
@@ -48,6 +79,7 @@ session_start();
                     <th>Auteur</th>
                     <th>Quantité</th>
                     <th>Prix</th>
+                    <th>Retirer des articles</th>
                 </tr>  
 
             <?php
@@ -59,6 +91,14 @@ session_start();
                     echo '<td>'.$livre['auteur'].'</td>';
                     echo '<td>'.$livre['qte'].'</td>';
                     echo '<td>'.$livre['qte']*$livre['prix'].'€</td>';
+                    echo '<td>
+                            <form method="POST">
+                                <input type = "hidden" name="idcmd" value="'.$livre['idcmd'].'">
+                                <input type = "hidden" name="idOuvrage" value="'.$livre['idouvrage'].'">
+                                <input type="number" step="1" name="qte" min = "0" max="'.$livre['qte'].'" required/>
+                                <input type="submit" name = "remove" value = "Retirer du panier">
+                            </form>
+                        </td>';
                     echo '</tr>';
                     $prixTotal += $livre['qte']*$livre['prix'];
                 } 
@@ -75,6 +115,11 @@ session_start();
             } else {
             ?>
                 Votre panier est vide.
+                <?php
+                    $requete = "DELETE FROM `commandes` WHERE idpersonne=".$_SESSION['user_id']." AND validee=0";
+                    $statement = $pdo->query($requete);
+
+                ?>
                 <div>
                     <img src="https://loulaa.e-monsite.com/medias/album/images/87072467chien-au-courses-jpg.jpg" alt="Chien avec un caddie vide."/>
                 </div>
